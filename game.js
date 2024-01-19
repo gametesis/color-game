4bd9898c19133b7d3d8112b5a1ca36165a9928d5
GameTesis <gametesis@gmail.com>
2024-01-19 22:59:03 +0000

    u

--- game.js
+++ game.js
@@ -108,7 +108,7 @@ function playCatchAudio() {
     if (playCount < maxPlays) {
       // Play the sound
       let sound = new Audio("/color-game/audio/Water Plop - Sound Effect (HD).mp3");
-      sound.volume = 0.5;
+      sound.volume = 0.8;
       sound.play();
 
       // Update play count and last play time
@@ -180,7 +180,7 @@ function playColorEvolutionAudio() {
       let sound = new Audio(
         "/color-game/audio/Lightsaber Ignition (Laser Sword) - Sound Effect for editing.mp3"
       );
-      sound.volume = 0.1;
+      sound.volume = 0.05;
       sound.play();
 
       // Update play count and last play time
@@ -215,7 +215,7 @@ function playColorDowngradeAudio() {
     if (playCount < maxPlays) {
       // Play the sound
       let sound = new Audio("/color-game/audio/Lightsaber Off.mp3");
-      sound.volume = 0.1;
+      sound.volume = 0.05;
       sound.play();
 
       // Update play count and last play time
